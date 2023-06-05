f7ea27fa3f379115f56c77218dde78298f5b024a
ascm00 <martin.aschermann2@gmail.com>
2023-06-05 10:49:57 +0200

    Fourth commit

--- signup.php
+++ signup.php
@@ -9,26 +9,34 @@
 	
     $email = htmlspecialchars(trim(@$_POST['email']));
     $password = @$_POST['password'];
+    $confirmPassowrd = @$_POST['confirmPassword'];
     $name = htmlspecialchars(trim(@$_POST['name']));
 
+    $correctPassword = false;
+    if($confirmPassowrd == $password){
+        $correctPassword = true;
 
-	  $passwordHash = password_hash($password, PASSWORD_DEFAULT); //pokud nemáte důvod to měnit, nechte heslo zahashovat výchozí funkcí; další možnosti viz manuál
-	
-    //uložíme uživatele do DB
-    $stmt = $db->prepare("INSERT INTO users(email, password, name) VALUES (?, ?, ?)");
-    $stmt->execute([$email, $passwordHash, $name]);
-	
-    //teď je uživatel uložen v DB - potřebujeme jeho id
-    //buď můžeme vzit id posledního záznamu přes last insert id, ale co když se to potká s více requesty? = ne zcela bezpečné
-    //lepší je načíst uživatele podle e-mailu = OK :)
-	
-    $stmt = $db->prepare("SELECT id FROM users WHERE email = ? LIMIT 1"); //limit 1 jen tu jen jako výkonnostní optimalizace
-    $stmt->execute([$email]);
-    //uživatele rovnou přihlásíme
-    $_SESSION['user_id'] = (int)$stmt->fetchColumn();
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT); //pokud nemáte důvod to měnit, nechte heslo zahashovat výchozí funkcí; další možnosti viz manuál
 
-	  //přesměrujeme uživatele na homepage
-  	header('Location: index.php');
+        //uložíme uživatele do DB
+        $stmt = $db->prepare("INSERT INTO users(email, password, name) VALUES (?, ?, ?)");
+        $stmt->execute([$email, $passwordHash, $name]);
+
+        //teď je uživatel uložen v DB - potřebujeme jeho id
+        //buď můžeme vzit id posledního záznamu přes last insert id, ale co když se to potká s více requesty? = ne zcela bezpečné
+        //lepší je načíst uživatele podle e-mailu = OK :)
+
+        $stmt = $db->prepare("SELECT id FROM users WHERE email = ? LIMIT 1"); //limit 1 jen tu jen jako výkonnostní optimalizace
+        $stmt->execute([$email]);
+        //uživatele rovnou přihlásíme
+        $_SESSION['user_id'] = (int)$stmt->fetchColumn();
+
+        //přesměrujeme uživatele na homepage
+        header('Location: index.php');
+
+    }
+
+
   }
 
 ?><!DOCTYPE html>
@@ -42,6 +50,13 @@
 
 <div class="container">
     <h2>Sign Up</h2>
+    <?php if(!$correctPassword){ ?>
+    <div class="container">
+        <div class="alert alert-warning" role="alert">
+            Passwords are not same.
+        </div>
+    </div>
+    <?php } ?>
     <form method="post">
         <div class="form-group">
             <label for="name">Name:</label>
